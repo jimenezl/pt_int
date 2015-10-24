dd1e70f07e3adef7d10f063fdab0d80a3e72793f
Candidate <candidate@example.com>
2015-10-24 21:04:42 +0000

    implemented 2

--- question_2/answer.sql
+++ question_2/answer.sql
@@ -1,5 +1,10 @@
 /* 	Write and save your SQL code in this file.
 	Do not rename or move it.  */
 
-select *
+select title, name
 from movie
+	join movie_actor
+		on movie.id = movie_actor.movie_id
+	join actor
+		on actor.id = movie_actor.actor_id
+order by title, name
