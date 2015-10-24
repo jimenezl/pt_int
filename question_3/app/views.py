4d0f488fd52a9b29d107c30109569b62e610e73a
Lisandro Jimenez <jimenezl@mit.edu>
2015-10-24 17:37:56 -0400

    fixed view

--- question_3/app/views.py
+++ question_3/app/views.py
@@ -46,7 +46,7 @@ def delete_movie(request, movie_id):
 def get_movie(request, movie_title):
     """Gets and displays a movie from the database"""
 
-    movie = Movies.objects.filter(id=movie_title)
+    movie = Movies.objects.filter(title=movie_title)
 
     #return id for Front-end handling
     return render(request, 'movie.html', {'movie': movie})
